0e73260f93e3421721858d9e586d4431ce78096c
mohitcofficial <mohitchandraofficial@gmail.com>
2024-08-06 12:47:25 +0530

    removed one unnecessary meta tag from layout.js file

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,10 +35,6 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="4QP5sU8e94-iZECe8pfBrj__ums-nt3y-9LELy8GVOA"
         />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
-        />
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-16609409855"
